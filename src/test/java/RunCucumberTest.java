8ee285e0b3ba6e6167ff28bfd04c22b205662cb4
maks13 <mrozhko13@gmail.com>
2020-10-25 21:58:38 +0200

    visual testing - renamed run class to RunCucumberTest.java

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,20 +1,22 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import utils.Common;
 
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        monochrome = true,
-        plugin = {"pretty"})
-public class CukesFeaturesRunnerTest {
+        plugin = {"pretty", "html:target/cucumber-html-report.html"})
+public class RunCucumberTest {
 
     @Before
     public void webDriverSetup() {
@@ -30,5 +32,10 @@ public class CukesFeaturesRunnerTest {
         } else {
             throw new IllegalArgumentException(String.format("Wrong browser specified - '%s'", Common.getConfigValue("browser")));
         }
+    }
+
+    @After
+    public void tearDown() {
+        getWebDriver().quit();
     }
 }
