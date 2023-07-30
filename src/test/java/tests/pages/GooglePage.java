e8a405b178ff129a709770216b49c05c52e2f116
maksr13 <mrozhko13@gmail.com>
2023-07-30 16:53:04 +0300

    Update GooglePage.java

--- src/test/java/tests/pages/GooglePage.java
+++ src/test/java/tests/pages/GooglePage.java
@@ -10,12 +10,10 @@ public class GooglePage {
     private By googleOfferedInTextLocator = By.cssSelector("div#SIvCob");
 
     public void clickOnGoogleLogo() {
-        $(googleLogo).waitUntil(Condition.visible, 15000);
         $(googleLogo).click();
     }
 
     public void clickOnGoogleOfferInText() {
-        $(googleOfferedInTextLocator).waitUntil(Condition.visible, 15000);
         $(googleOfferedInTextLocator).click();
     }
 }
